812fc7b3bd9007fbee1e851b99b6468da1df13e4
Siddhant Khare <siddhantkhare056@gmail.com>
2025-01-17 00:58:24 +0530

    add vite pwa

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,23 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import { VitePWA } from "vite-plugin-pwa";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      manifest: {
+        name: "Diskettes",
+        short_name: "Diskettes",
+        description: "Code editor and sharing platform",
+        theme_color: "#ffffff",
+        background_color: "#ffffff",
+        display: "standalone",
+        start_url: "/",
+        icons: [{ src: "avatar.png", sizes: "512x512", type: "image/png" }],
+      },
+    }),
+  ],
+});
